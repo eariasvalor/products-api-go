0bd686eb990394f8f5412e68615a75a2aa829d2d
eariasvalor <eariasvalor@gmail.com>
2026-05-20 21:05:19 +0200

    test: add product service mock

---
+++ internal/mocks/product_service_mock.go
@@ -0,0 +1,48 @@
+package mocks
+
+import (
+	"proyectoGo/internal/domain"
+
+	"github.com/stretchr/testify/mock"
+)
+
+type ProductServiceMock struct {
+	mock.Mock
+}
+
+func (m *ProductServiceMock) GetAll() ([]*domain.Product, error) {
+	args := m.Called()
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).([]*domain.Product), args.Error(1)
+}
+
+func (m *ProductServiceMock) GetByID(id int) (*domain.Product, error) {
+	args := m.Called(id)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*domain.Product), args.Error(1)
+}
+
+func (m *ProductServiceMock) Create(product *domain.Product) (*domain.Product, error) {
+	args := m.Called(product)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*domain.Product), args.Error(1)
+}
+
+func (m *ProductServiceMock) Update(id int, product *domain.Product) (*domain.Product, error) {
+	args := m.Called(id, product)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*domain.Product), args.Error(1)
+}
+
+func (m *ProductServiceMock) Delete(id int) error {
+	args := m.Called(id)
+	return args.Error(0)
+}
