94a819cb5e5cde39155d4351984944cb0d0135aa
eariasvalor <eariasvalor@gmail.com>
2026-05-20 21:47:20 +0200

    feat: add binding validation tags to product struct

--- internal/domain/product.go
+++ internal/domain/product.go
@@ -4,10 +4,25 @@ import "errors"
 
 type Product struct {
 	ID          int     `json:"id"`
-	Name        string  `json:"name"`
+	Name        string  `json:"name" binding:"required,min=2,max=100"`
 	Description string  `json:"description"`
-	Price       float64 `json:"price"`
-	Stock       int     `json:"stock"`
+	Price       float64 `json:"price" binding:"required,gt=0"`
+	Stock       int     `json:"stock" binding:"min=0"`
+}
+
+func NewProduct(name string, description string, price float64, stock int) (*Product, error) {
+	p := &Product{
+		Name:        name,
+		Description: description,
+		Price:       price,
+		Stock:       stock,
+	}
+
+	if err := p.Validate(); err != nil {
+		return nil, err
+	}
+
+	return p, nil
 }
 
 func (p *Product) Validate() error {
